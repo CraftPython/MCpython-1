31046a996dd6dc312af711ecbd8793d648ba9cad
CraftPython <pi5qu4ed@gmail.com>
2013-02-21 11:22:09 +0000

    Create MCpython-1.py
    A python script for MC Pi Edition.

---
+++ MCpython-1.py
@@ -0,0 +1,16 @@
+import mcpi.minecraft as minecraft
+import mcpi.block as block
+import math
+
+mc = minecraft.Minecraft.create()
+loc = mc.player.getPos()
+trigx = 0
+trigy = 50
+trigz = 0
+x = loc.x
+y = loc.y
+z = loc.z
+if  x = trigx and
+    y = trigy - 1 and
+    z = trigz then
+        mc.postToChat("You found it")
